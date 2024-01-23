75d26cc62e5c09d4c218ee5eb4d921ec5b2b878f
DaviAieta <aieta.davi@gmail.com>
2024-01-23 04:03:41 -0300

    commit

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,11 +4,11 @@ import User from '../models/users.models'
 class UserController {
   static async listUsers(req: Request, res: Response) {
     try {
-      const users = await User.find()
+      const users = await User.findAll()
 
-      res.status(200).json({ users })
+      res.render('users', { users })
     } catch (error) {
-      res.status(500).json({ error: 'error at listUsers' })
+      res.status(500).json('error at find users')
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from 'express'
 import userRoutes from './routes/user.routes'
+import * as exphbs from 'express-handlebars';
+import path from 'path'
 
 const app = express()
 const port = 3000
+
+const handlebars = exphbs.create({
+  defaultLayout: 'main'
+})
+
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+
+// const caminho_view = path.join(__dirname, '/views')
+// console.log(caminho_view)
+app.set('views', path.join(__dirname, '/views'));
 
 app.use('/users', userRoutes)
 
